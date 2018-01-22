cc0392f4c2c9302740ea2d03e7519b437431873f
Ryan <ryanwesterberg@gmail.com>
2018-01-22 10:55:21 -0600

    robbery/jumped/mugged

--- algorithms.js
+++ algorithms.js
@@ -62,4 +62,43 @@ function ConflictExpert({ reason, weapons_at_scene, shots_fired }, callback) {
     }
 }
 
+function StrategyExpert({ strategy }, callback) {
+    const experts = []
+    const cache = {}
+    getData(handleData, done)
+
+    function handleData(obj) {
+        let filtered = (
+            obj.name === 'Chicago' &&
+            (
+                obj.outcome === 'Conflict resolved' ||
+                obj.outcome === 'ConflictÂ resolvedÂ asÂ longÂ asÂ certainÂ conditionsÂ areÂ met' ||
+                obj.outcome === 'Conflict resolved temporarily'
+            ) &&
+            obj[strategy] === 'Very Effective'
+        )
+        if (filtered) {
+            if (!cache[obj.id]){
+                cache[obj.id] = experts.length
+                return experts.push({
+                    id: obj.id,
+                    count: 1,
+                    first_name: obj.first_name,
+                    last_name: obj.last_name
+                })
+            }
+            let pos = cache[obj.id]
+            experts[pos].count++
+        }
+    }
+
+    function done(err) {
+        if (err) return console.log(err)
+        experts.sort((a, b) => {
+            return b.count - a.count
+        })
+        return callback(experts)
+    }
+}
+
 export default ConflictExpert
